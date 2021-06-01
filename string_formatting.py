9f465b9a34bb4a51affccfeb08ea78bb20c7738e
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-02 01:29:11 +1000

    Create string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,13 @@
+# leaning python the hard way
+
+# learnt the basics
+# string formatting using f
+
+first_name = 'Malav'
+last_name = 'Parikh'
+middle_name = 'Arunkumar'
+
+print(f"My first name is {first_name}")
+print(f"My last name is {last_name}")
+print(f"My middle name is {middle_name}")
+print(f"My full name is {first_name} {middle_name} {last_name}")
