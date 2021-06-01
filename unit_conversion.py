2bde41b737ba99051da4b86b27ee07013f3550db
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-02 01:45:55 +1000

    Create unit_conversion.py

---
+++ unit_conversion.py
@@ -0,0 +1,13 @@
+# converting units 
+# centimeters to inches
+# pounds to kgs
+
+
+my_weight = 120 # in lbs
+my_height = 180 # in cm
+
+# without conversion
+print(f"My weight is {my_weight} lbs and my height is {my_height} cm")
+
+# after conversion
+print(f"My weight is {my_weight/2.20462262} kgs and my height is {my_height*0.39370079} inches")
