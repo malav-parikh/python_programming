321f558551d9831d38ef23c0995c37c36f8a4952
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-02 12:27:02 +1000

    Create Escape sequences.py

---
+++ Escape sequences.py
@@ -0,0 +1,27 @@
+# leaning python the hard way
+
+# learnt the basics
+
+# escape sequences
+
+# using quotes inside quotes
+print("I am 5'11\" tall")
+print('I am 5\'11" tall')
+
+# other escape sequenes \t \n \\
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* cat food
+\t* fish
+\t* catnip\n\t* grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
