0fdebcc40145cec6f97adab11d6c424e68400247
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-02 12:27:17 +1000

    Create some more printing.py

---
+++ some more printing.py
@@ -0,0 +1,18 @@
+# leaning python the hard way
+
+# learnt the basics
+
+# some more printing stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There is something
+wrong with this text
+or not
+Are triple quotes allowed?
+""")
