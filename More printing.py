3115356195f3625491f61155b79114db8d7e28b7
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-02 12:27:14 +1000

    Create More printing.py

---
+++ More printing.py
@@ -0,0 +1,37 @@
+# leaning python the hard way
+
+# learnt the basics
+
+# More printing exercise
+
+print("Mary had a little lamb.")
+print("Its fleece was white as {}".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "M"
+end2 = "a"
+end3 = "c"
+end4 = "A"
+end5 = "n"
+end6 = "d"
+end7 = "C"
+end8 = "h"
+end9 = "e"
+end10 = "e"
+end11 = "s"
+end12 = "e"
+
+# prints on different lines
+print(end1 + end2 + end3)
+print(end4 + end5 + end6)
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# prints on same line with a space in between using end = ' '
+print(end1 + end2 + end3, end = ' ')
+print(end4 + end5 + end6, end = ' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+print('But....')
+print(f"My fave food is {'pizza'}")
+
