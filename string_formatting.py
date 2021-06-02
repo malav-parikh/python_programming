2557991c7da93118812e06799f00f334285c9345
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-02 11:19:25 +1000

    Update string_formatting.py

--- string_formatting.py
+++ string_formatting.py
@@ -1,6 +1,7 @@
 # leaning python the hard way
 
 # learnt the basics
+
 # string formatting using f
 
 first_name = 'Malav'
@@ -11,3 +12,9 @@ print(f"My first name is {first_name}")
 print(f"My last name is {last_name}")
 print(f"My middle name is {middle_name}")
 print(f"My full name is {first_name} {middle_name} {last_name}")
+
+# string formatting using format function
+
+sentence = "My full name is {} {} {}"
+
+print(sentence.format(first_name,middle_name,last_name))
