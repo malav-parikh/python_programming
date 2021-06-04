f1fcf9fdbcc4252d96ba60b82f1870a2488d7629
malav1 <75304318+malav1@users.noreply.github.com>
2021-06-04 11:29:42 +1000

    Create prompting input.py

---
+++ prompting input.py
@@ -0,0 +1,11 @@
+# leaning python the hard way
+
+# learnt the basics
+
+# Prompting input from user
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"so you are {age} years old, {height} tall and {weight} kgs heavy")
